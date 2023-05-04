60f7105072d2041b3adfa219515ae7dd2483b130
UGRSJ <85352429+UGRSJ@users.noreply.github.com>
2023-05-04 13:37:58 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+// html과 자바스크립 확인하며, 필요에 따라 자바스크립트에서는 변수 선언 후, 함수 // html에서는 자바스크립트로 불러오기 위해 id값 부여
 //랜덤번호지정
 //유저가 번호를 입력한다 그리고 go라는 버튼을 누른다
 // 만약에 유저가 랜덤 번호를 맞추면, 맞췄습니다!
@@ -8,10 +10,10 @@
 //유저가 1~100 범위 밖에 숫자를 입력하면 아렬준다. 기회를 깍지 않는다.
 // 유저가 이미 입력한 숫자를 또 입력하면, 알려준다, 기회를 깍지 않는다.
 //유저 인풋 입력 후, 인풋 값에 커서 넣으면 이전값 제거하기
-//
+
 
 let computerNum = 0;
-let playButton = document.getElementById("playButton");
+let playButton = document.getElementById("playButton"); // 클릭 시 자바스크립트로 이벤트 실행을 위해 html에 있는 플레이버튼을 자바스크립트로 불러온다 > html에 id를 주어 자바스크립트로 불러옴 > 변수 선언 > 이벤트리스ㅌ너 추가 > play 함수 만들기 >if문 활요하여 유저와 컴퓨터 값 비교
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
 let resetButton = document.getElementById("resetButton");
